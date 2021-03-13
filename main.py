ef251b791d9090a45b57a7d20df9573560409ac3
c10coding <caleb.ja.owens@gmail.com>
2021-03-13 14:17:24 -0500

    is_promotion works

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def dif_oddsumcount(n):
     if n == 0:
         return 0
@@ -8,10 +7,16 @@ def dif_oddsumcount(n):
         for i in range(1, n + 1, 2):
             odd_numbers.append(i)
 
-        print(odd_numbers)
         num_odd_numbers = len(odd_numbers)
         sum_odd_numbers = 0
         for num in odd_numbers:
             sum_odd_numbers += num
 
         return abs(sum_odd_numbers - num_odd_numbers)
+
+
+def is_promotion(cake, promotions):
+    for str in promotions:
+        if (cake == str):
+            return True
+    return False
