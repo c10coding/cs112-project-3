76156a6225743896260384fa9e2a1dd5a8f4a3aa
c10coding <caleb.ja.owens@gmail.com>
2021-03-13 14:32:05 -0500

    biggest_sale is done

--- main.py
+++ main.py
@@ -20,3 +20,27 @@ def is_promotion(cake, promotions):
         if (cake == str):
             return True
     return False
+
+
+def biggest_sale(cakes, sales, flavor_cakes):
+    if len(cakes) == 0 or len(sales) == 0:
+        return None
+    else:
+
+        # Gets the highest sale
+        highest_sale = 0
+        index_highest_sale = 0
+        for i in range(len(sales)):
+            sale = sales[i]
+            if highest_sale < sale:
+                highest_sale = sale
+                index_highest_sale = i
+
+        most_sold_cake = cakes[index_highest_sale]
+        # Now that we have the most sold cake, we check to see if flavor_cakes has it.
+        # If it doesn't have it, the loop essentially does nothing and just returns marble afterwards.
+        for flavor in flavor_cakes:
+            if most_sold_cake == flavor:
+                return "single"
+
+        return "marble"
